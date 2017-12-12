c9b45639a29c4563bf7f9be0a9cc6217ec888f0e
Aidan <stein.aidan@gmail.com>
2017-12-11 19:19:17 -0500

    bugfixes

--- run.sh
+++ run.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # install requirements to run playbook
-sudo apt-get install aptitude python3-pip python-pip git software-properties-common
+sudo apt-get install aptitude python-pip python3-pip git software-properties-common;
 
 # install ansible
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install ansible
+sudo apt-add-repository ppa:ansible/ansible;
+sudo apt-get update;
+sudo apt-get install ansible;
 
 # get full playbook repository
-cd ~
-mkdir -p setup
-cd setup
-git clone https://github.com/steinad/provision-local.git
-cd provision-local
+cd ~;
+mkdir -p setup;
+cd setup;
+git clone https://github.com/steinad/provision-local.git;
+cd provision-local;
 
 # install python requirements for playbook
-sudo pip install -r requirements.txt
-sudo pip2 install -r requirements.txt
+sudo pip install -r requirements.txt;
+sudo pip2 install -r requirements.txt;
 
 echo -e "Edit the config.json file, and [ENTER] when finished" && read
 
